e5707671b22f677a77147a41ad30d72bcde236cd
kovaciardey <andrei.kovaci@gmail.com>
2024-10-06 18:57:02 +0100

    built the snooker table * arranged the balls at the start of the game * made prefab for the cue ball * the cue ball uses the icosphere graphic now

--- Assets/Scripts/RandomColour.cs
+++ Assets/Scripts/RandomColour.cs
@@ -1,25 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class RandomColour : MonoBehaviour
 {
     private Color _initialColor;
-    private Renderer _renderer;
+    public Renderer ballRenderer;
     
     // Start is called before the first frame update
     void Start()
     {
-        // Get the Renderer component from the GameObject
-        _renderer = GetComponent<Renderer>();
-
-        if (_renderer != null)
+        if (ballRenderer != null)
         {
             // Generate a random color
             _initialColor = new Color(Random.value, Random.value, Random.value);
 
             // Set the GameObject's material color to the random color
-            _renderer.material.color = _initialColor;
+            ballRenderer.material.color = _initialColor;
         }
         else
         {
@@ -32,11 +30,11 @@ public class RandomColour : MonoBehaviour
         Color negativeColor = new Color(1.0f - _initialColor.r, 1.0f - _initialColor.g, 1.0f - _initialColor.b);
         
         // Set the GameObject's material color to the random color
-        _renderer.material.color = negativeColor;
+        ballRenderer.material.color = negativeColor;
     }
 
     public void ResetColour()
     {
-        _renderer.material.color = _initialColor;
+        ballRenderer.material.color = _initialColor;
     }
 }
